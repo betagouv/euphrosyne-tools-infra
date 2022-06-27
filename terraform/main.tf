71f98ee959c4caddfcf7e193193a7e10a245128b
Maxime Cattet <maximecattet@gmx.com>
2022-06-27 08:01:54 -0400

    feat: Add storage share for commons and elab upload
    Also add more subnet to access the storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,6 +81,7 @@ resource "azurerm_subnet" "guacsubnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
 
   delegation {
     name = "delegation"
