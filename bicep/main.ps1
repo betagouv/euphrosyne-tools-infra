bf5b0ffc3dd52400ac364b4075385f9295da4cf1
witold <witoldwrob@gmail.com>
2023-02-13 12:00:22 +0100

    TO DELETE: remove prevent drive cmds (test)

--- bicep/main.ps1
+++ bicep/main.ps1
@@ -23,5 +23,5 @@ Enable-PSRemoting -Force
 .\psexec -u $AccountName -p $AccountPassword -accepteula -h -i "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" -File $(pwd)\mountDrive.ps1 -FileShare $FileShare -StorageAccountAccessKey $StorageAccountAccessKey -StorageAccount $StorageAccount -FileShareProjectFolder $FileShareProjectFolder
 
 # Prevent drive mapping for all users
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCMD" -Value "1" -Type DWORD
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDrives" -Value "1" -Type DWORD
+# Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCMD" -Value "1" -Type DWORD
+# Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDrives" -Value "1" -Type DWORD
