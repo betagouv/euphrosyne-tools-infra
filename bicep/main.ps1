3df958011c1f1076e202341941113d8537f40a81
witold <witoldwrob@gmail.com>
2023-02-13 16:00:14 +0100

    fix: add missing quotes in main script

--- bicep/main.ps1
+++ bicep/main.ps1
@@ -20,7 +20,7 @@ param(
 
 # Mount fileshare for user
 Enable-PSRemoting -Force
-.\psexec -u $AccountName -p $AccountPassword -accepteula -h -i "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File $(pwd)\mountDrive.ps1 -FileShare $FileShare -StorageAccountAccessKey $StorageAccountAccessKey -StorageAccount $StorageAccount -FileShareProjectFolder $FileShareProjectFolder
+.\psexec -u $AccountName -p $AccountPassword -accepteula -h -i "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "$(pwd)\mountDrive.ps1" -FileShare $FileShare -StorageAccountAccessKey $StorageAccountAccessKey -StorageAccount $StorageAccount -FileShareProjectFolder $FileShareProjectFolder
 
 # Prevent drive mapping for all users
 # Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCMD" -Value "1" -Type DWORD
